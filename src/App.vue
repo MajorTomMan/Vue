22480d5ac3fbdacfdff229e3c82484a258b65b03
MajorTomMan <765719516@qq.com>
2024-11-10 22:47:18 +0800

    项目初始化和增加依赖以及配置文件

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Date: 2024-11-10 20:53:50
+ * @LastEditors: MajorTomMan 765719516@qq.com
+ * @LastEditTime: 2024-11-10 20:57:57
+ * @FilePath: \Vue\project\src\App.vue
+ * @Description: MajorTomMan @版权声明 保留文件所有权利
+-->
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
