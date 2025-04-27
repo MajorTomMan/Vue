00d7248ca9f766082a04222b73a72a7a10c7d297
MajorTomMan <765719516@qq.com>
2025-04-27 22:18:52 +0800

    修改后台管理系统的依赖,整合element-plus

--- simple-backend-manager-system/vite.config.ts
+++ simple-backend-manager-system/vite.config.ts
@@ -1,20 +1,35 @@
 /*
  * @Date: 2025-04-20 12:53:34
  * @LastEditors: MajorTomMan 765719516@qq.com
- * @LastEditTime: 2025-04-20 19:16:25
- * @FilePath: \simple-backend-manager-system\vite.config.ts
+ * @LastEditTime: 2025-04-27 22:16:40
+ * @FilePath: \Vue\simple-backend-manager-system\vite.config.ts
  * @Description: MajorTomMan @版权声明 保留文件所有权利
  */
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import vueDevTools from 'vite-plugin-vue-devtools'
+import ElementPlus from 'unplugin-element-plus/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    ElementPlus(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
-
-declare module '*.vue' {
-  import { ComponentOptions } from 'vue'
-  const componentOptions: ComponentOptions
-  export default componentOptions
-}
